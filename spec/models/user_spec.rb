24c6019dc4618e52092de12778c14d4a2fd5e0ef
shigeki310 <hinahisa01240301@gmail.com>
2022-07-20 10:27:05 +0900

    Rubocopの実行

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe User, type: :model do
 
   describe 'ユーザー新規登録' do
     context '新規登録できる場合' do
-      it "全ての項目の条件を満たしていれば登録できる" do
+      it '全ての項目の条件を満たしていれば登録できる' do
         expect(@user).to be_valid
       end
     end
@@ -38,24 +38,24 @@ RSpec.describe User, type: :model do
         @user.password = '12345'
         @user.password_confirmation = '12345'
         @user.valid?
-        expect(@user.errors.full_messages).to include "Password is too short (minimum is 6 characters)"
+        expect(@user.errors.full_messages).to include 'Password is too short (minimum is 6 characters)'
       end
       it 'passwordが129文字以上では登録できない' do
         @user.password = Faker::Internet.password(min_length: 129)
         @user.password_confirmation = @user.password
         @user.valid?
-        expect(@user.errors.full_messages).to include "Password is too long (maximum is 128 characters)"
+        expect(@user.errors.full_messages).to include 'Password is too long (maximum is 128 characters)'
       end
       it '重複したemailが存在する場合は登録できない' do
         @user.save
         another_user = FactoryBot.build(:user, email: @user.email)
         another_user.valid?
-        expect(another_user.errors.full_messages).to include "Email has already been taken"
+        expect(another_user.errors.full_messages).to include 'Email has already been taken'
       end
       it 'emailは@を含まないと登録できない' do
         @user.email = 'testmail'
         @user.valid?
-        expect(@user.errors.full_messages).to include "Email is invalid"
+        expect(@user.errors.full_messages).to include 'Email is invalid'
       end
       it 'family_nameが空では登録できない' do
         @user.family_name = ''
@@ -80,22 +80,22 @@ RSpec.describe User, type: :model do
       it 'family_nameは全角(漢字・ひらがな・カタカナ)でなければ登録できない' do
         @user.family_name = 'kana'
         @user.valid?
-        expect(@user.errors.full_messages).to include "Family name is invalid"
+        expect(@user.errors.full_messages).to include 'Family name is invalid'
       end
       it 'given_nameは全角(漢字・ひらがな・カタカナ)でなければ登録できない' do
         @user.given_name = 'kana'
         @user.valid?
-        expect(@user.errors.full_messages).to include "Given name is invalid"
+        expect(@user.errors.full_messages).to include 'Given name is invalid'
       end
       it 'family_name_kanaは全角(カタカナ)でなければ登録できない' do
         @user.family_name_kana = 'かな'
         @user.valid?
-        expect(@user.errors.full_messages).to include "Family name kana is invalid"
+        expect(@user.errors.full_messages).to include 'Family name kana is invalid'
       end
       it 'given_name_kanaが全角(カタカナ)でなければ登録できない' do
         @user.given_name_kana = 'かな'
         @user.valid?
-        expect(@user.errors.full_messages).to include "Given name kana is invalid"
+        expect(@user.errors.full_messages).to include 'Given name kana is invalid'
       end
       it 'birthdayが空では登録できない' do
         @user.birthday = ''
