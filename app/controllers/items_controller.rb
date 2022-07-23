5418d649b60f57c49ad87771cf7bf8f395bc3c0d
shigeki310 <hinahisa01240301@gmail.com>
2022-07-23 17:31:23 +0900

    コードの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, except: [:index, :new, :create]
-  before_action :authenticate_user!, expect: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :update] 
 
   def index
