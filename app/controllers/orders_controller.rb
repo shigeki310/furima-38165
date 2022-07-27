5023a7aabe651a053dc8ec7397bdfb07821b9697
shigeki310 <hinahisa01240301@gmail.com>
2022-07-27 18:03:17 +0900

    ordersコントローラーの編集

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,27 @@
+class OrdersController < ApplicationController
+
+  def index
+    @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :city, :prefecture_id, :block, :building, :phone_number).merge(user_id: current_user.id,item_id: params[:item_id])
+  end
+
+
+
+end
